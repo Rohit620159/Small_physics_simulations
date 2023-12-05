43b58f693a7b94fe68c0f14e7861967d9c6a9a82
Rohit Raj <103591017+Rohit620159@users.noreply.github.com>
2023-12-05 23:30:11 +0000

    Update Gravity_influenced_motion.py

--- Gravity_influenced_motion.py
+++ Gravity_influenced_motion.py
@@ -1,1 +1,13 @@
 import numpy as np
+
+G = 6.674 * 10**-11
+def gravitational_force(m ,n, r):
+  F = (G*m*n)/(r**2)
+  return F
+  m = 5
+  n = 3
+  r = 2
+  force = gravitational_force(m, n, r)
+
+# Print the result
+print("Gravitational Force:", force)
